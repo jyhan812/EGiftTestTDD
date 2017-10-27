19970494b5c9ea06e9c8b605ba7a2391dc264960
jyhan812 <32969446+jyhan812@users.noreply.github.com>
2017-10-26 22:31:14 -0400

    Added comments for logic using Stack

--- MatchParenthesis/MatchClass.cs
+++ MatchParenthesis/MatchClass.cs
@@ -13,8 +13,12 @@ namespace MatchParenthesis
         {
             bool isMatched = false;
 
+            var stack = new Stack<string>();
 
-
+            //logic
+            //1.when you see ( push it and pop stack when you see ) 
+            //2.if stack has no ( but if ) entered, it fails
+            //3.finally when the string is parsed completely the stack should be empty
 
 
 
