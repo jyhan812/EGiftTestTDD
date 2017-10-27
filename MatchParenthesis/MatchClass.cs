a4292fedb4186daf682f7d0977e77d550b61e2c8
jyhan812 <32969446+jyhan812@users.noreply.github.com>
2017-10-26 22:45:16 -0400

    Added 'for' iterator through input string to read each character

--- MatchParenthesis/MatchClass.cs
+++ MatchParenthesis/MatchClass.cs
@@ -13,14 +13,18 @@ namespace MatchParenthesis
         {
             bool isMatched = false;
 
-            var stack = new Stack<string>();
+            Stack<string> stack = new Stack<string>();
 
             //logic
             //1.when you see ( push it and pop stack when you see ) 
             //2.if stack has no ( but if ) entered, it fails
             //3.finally when the string is parsed completely the stack should be empty
 
+            for(int i = 0; i < input.Length; i++)
+            {
 
+
+            }
 
             return isMatched;
         }
